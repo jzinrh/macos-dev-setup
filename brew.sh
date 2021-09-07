b237ba5739e00d40251bbd84a5baebe982cb7fa6
jzinrh <jzinrh@gmail.com>
2021-09-07 08:03:43 -0400

    Update brew.sh
    add brew to path

--- brew.sh
+++ brew.sh
@@ -7,6 +7,9 @@ headerBig "Preparing Homebrew"
 if ! [ -x "$(command -v brew)" ]; then
   headerSmall "Installing brew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  headerSmall "Linking brew"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew update
